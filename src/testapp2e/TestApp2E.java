7a1fb39fd2bb18796f80fee120d239c7d2548eda
keycre12 <bolambotkeycee@gmail.com>
2024-09-25 17:52:03 +0800

    first commit

---
+++ src/testapp2e/TestApp2E.java
@@ -0,0 +1,51 @@
+package testapp2e;
+
+import java.sql.*;
+import java.util.*;
+
+public class TestApp2E {
+    
+    //Connection Method to SQLITE
+    public static Connection connectDB() {
+            Connection con = null;
+            try {
+                Class.forName("org.sqlite.JDBC"); // Load the SQLite JDBC driver
+                con = DriverManager.getConnection("jdbc:sqlite:sample.db"); // Establish connection
+                System.out.println("Connection Successful");
+            } catch (Exception e) {
+                System.out.println("Connection Failed: " + e);
+            }
+            return con;
+        }
+
+    public static void main(String[] args) {
+        connectDB();
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter First Name: ");
+        String fname = sc.nextLine();
+        System.out.print("Enter Last Name: ");
+        String lname = sc.nextLine();
+        System.out.print("Enter Email: ");
+        String email = sc.next();
+        System.out.print("Enter Status: ");
+        String status = sc.next();
+        
+        String sql = "INSERT INTO Students (s_fname, s_lname, s_email, s_status) VALUES (?, ?, ?, ?)";  
+        
+        try{
+            Connection con = connectDB();
+            PreparedStatement state = con.prepareStatement(sql);
+            
+            state.setString(1, fname);
+            state.setString(2, lname);
+            state.setString(3, email);
+            state.setString(4, status);
+            state.executeUpdate();
+            
+            System.out.println("Insertion Successful.");
+        }catch(SQLException e){
+            System.out.println("Connection Error: "+e);
+        }
+    }
+}
